fbb735e8e6dac14d6e84c1ff8eeaac09a9f92cce
André Souza Mattos <andrexpsouza@gmail.com>
2026-04-21 16:04:09 -0300

    praticas03 finalizada

---
+++ praticas/praticas03/conta.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "conta.h"
+
+Conta conta_criar(int numero)
+{
+    Conta conta;
+    return conta;
+}
+
+void conta_depositar(Conta *conta, double valor)
+{
+    conta -> saldo += valor;
+}
+
+void conta_sacar(Conta *conta, double valor)
+{
+    if (conta -> saldo >= valor)
+    {
+        conta -> saldo -= valor;
+    }
+}
+
+double consultar_saldo(Conta *conta)
+{
+    return conta -> saldo;
+}
